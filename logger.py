b3d98f54f2253e282b16bec14523b3cb66a71b91
Tatu Peltola <tatu.peltola@gmail.com>
2022-07-25 01:50:05 +0300

    Make logger.py easier to extend

--- logger.py
+++ logger.py
@@ -26,35 +26,52 @@ class InfluxdbLogger:
             point = point.field(name, value)
         self.write_api.write(self.bucket, self.org, point)
 
+# TODO: move CSV writing here to a writer class
 
-def influxdb_main(measurement, influxdb_queue):
-    """Thread to communicate with the InfluxDB server.
 
-    This is done in a separate thread to avoid long pauses in the
+def writer_main(measurement, writer_class, writer_queue):
+    """Main function of a thread that stores the data somewhere.
+    Where the data is stored depends on the writer class given.
+
+    Data is written in a separate thread to avoid long pauses in the
     measurement thread in case writing the data takes some time.
     """
 
     influxdb_logger = InfluxdbLogger(measurement)
+    writer = writer_class(measurement)
     while True:
-        time, fields = influxdb_queue.get()
-        influxdb_logger.write(time, fields)
-        influxdb_queue.task_done()
+        time, fields = writer_queue.get()
+        writer.write(time, fields)
+        writer_queue.task_done()
 
 
 class Logger:
-    def __init__(self, measurement):
-        self.influxdb_queue = queue.Queue(maxsize=100)
+    def __init__(self, measurement, enable_influxdb=True):
+        self.queues = list()
+
+        if enable_influxdb:
+            self.start_writer(measurement, InfluxdbLogger)
+
+    def start_writer(self, measurement, writer_class):
+        """Start a writer thread and create a queue
+        to communicate with the thread.
+        """
+        q = queue.Queue(maxsize=100)
         self.influxdb_thread = threading.Thread(
-            target = influxdb_main,
-            args = (measurement, self.influxdb_queue),
+            target = writer_main,
+            args = (measurement, writer_class, q),
         )
         self.influxdb_thread.start()
-
+        self.queues.append(q)
 
     def write(self, time, fields):
+        """Store a measurement record.
+        """
+        # Send the data to each writer thread through a queue.
         # Never block here to make sure the measurement code keeps running.
         # If a queue gets full for some reason, discard the data.
-        try:
-            self.influxdb_queue.put((time, fields), block=False)
-        except queue.Full:
-            pass
+        for q in self.queues:
+            try:
+                q.put((time, fields), block=False)
+            except queue.Full:
+                pass
